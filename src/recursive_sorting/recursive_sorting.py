4647db07d72373978a693b0e7e3d10e26ad780ee
Rod <rod.bromberg@yahoo.com>
2020-03-03 20:07:26 -0500

    day-1 mvp

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,30 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    
-    return merged_arr
+def merge(arrA, arrB):
+    result = []
+    while(len(arrA) or len(arrB)):
+        if len(arrA) or len(arrB):
+            if(arrA[0] < arrB[0]):
+                result.append(arrA.pop(0))
+            else:
+                result.append(arrB.pop(0))
+        elif(len(arrB)):
+            result.append(arrB.pop(0))
+        else:
+            result.append(arrA.pop(0))
+    return result
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    middle = len(arr) // 2
+    left = arr[:middle]
+    right = arr[middle:]
+    return merge(merge_sort(left), merge_sort(right))
 
-    return arr
+
+print(merge_sort([2, 1, 3, 5]))
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -20,7 +33,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +42,24 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
+
+
+#     def merge(arrA, arrB):
+#         elements = len(arrA) + len(arrB)
+#     merged_arr = [0] * elements
+#     # TO-DO
+
+#     return merged_arr
+
+
+# # TO-DO: implement the Merge Sort function below USING RECURSION
+# def merge_sort(arr):
+#     if arr(len) <= 1:
+#         return arr
+#     middle = arr(len) / 2
+#     left = arr[0:middle]
+#     right = arr[middle:arr(len)]
+#     return merge(merge_sort(left), merge_sort(right))
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,4 +1,4 @@
-# STRETCH: implement Linear Searchs
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
 
   # TO-DO: add missing code
@@ -7,20 +7,39 @@ def linear_search(arr, target):
 
 
 # STRETCH: write an iterative implementation of Binary Search
+# def binary_search(arr, target):
+
+#     if len(arr) == 0:
+#         return -1  # array empty
+
+#     low = 0
+#     high = len(arr)-1
+
+#     # TO-DO: add missing code
+
+#     return -1  # not found
+
+
 def binary_search(arr, target):
-
-    if len(arr) == 0:
-        return -1  # array empty
-
     low = 0
-    high = len(arr)-1
-
-    # TO-DO: add missing code
-
-    return -1  # not found
+    high = len(arr) - 1
+    while(low <= high):
+        middle = low + (low + high) // 2
+        guess = arr[middle]
+        if(guess == target):
+            return middle
+        else:
+            if(target < guess):
+                high = middle - 1
+            else:
+                low = middle + 1
+    return - 1
 
 
 # STRETCH: write a recursive implementation of Binary Search
+print(binary_search([1, 2, 3, 4, 5], 3))
+
+
 def binary_search_recursive(arr, target, low, high):
 
     middle = (low+high)//2
